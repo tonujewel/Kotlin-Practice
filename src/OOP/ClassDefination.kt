975d044d39049dbce63ccbc574be8348532f3450
Jewel Rana <tonujewel@gmail.com>
2019-11-25 23:30:23 +0600

    *double dot *REPL *String interpolation

---
+++ src/OOP/ClassDefination.kt
@@ -0,0 +1,18 @@
+package OOP
+
+fun main(){
+
+    var student = Student("Jewel Rana")
+
+
+
+}
+
+class Student (name:String){
+
+    var name:String = "Dymmy"
+
+    init {
+        println("init Student Name is ${name}")
+    }
+}
